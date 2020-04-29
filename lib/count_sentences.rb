714d7ca8797867d932f698e02e0f5c4bc8c8ae7d
Thomas John Helin <thomashelin@yahoo.com>
2020-04-29 19:59:31 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    #self.split(/\?|\.|!/)
+    array = self.split(/[?!.]/)
+    array.reject{ |e| e.size < 2 }.length
+    #self.split(/[?!.]\s+[A-Z]/).length.reject
+    #binding.pry
   end
 end
