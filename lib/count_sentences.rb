b4cbd71be4087c0802735cf99e1039635eb12240
Thomas John Helin <thomashelin@yahoo.com>
2020-04-29 19:36:56 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,7 @@ class String
 
   def count_sentences
     #self.split(/\?|\.|!/)
-    self.split.reject(/\w[?!.]/).length
+    self.split(/\w[?!.]/).length
     #self.split(/[?!.]\s+[A-Z]/).length.reject
     binding.pry
   end
