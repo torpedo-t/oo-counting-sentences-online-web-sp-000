03f8bc464ae71d8d409b4fcf258d21b754ab3703
Thomas John Helin <thomashelin@yahoo.com>
2020-04-29 19:54:13 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,7 +17,7 @@ class String
   def count_sentences
     #self.split(/\?|\.|!/)
     array = self.split(/[?!.]/)
-    array.reject{ |e| e.size < 2 }
+    array.reject{ |e| e.size < 2 }.length
     #self.split(/[?!.]\s+[A-Z]/).length.reject
     #binding.pry
   end
